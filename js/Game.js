1e19770ade2ab6c8d420bd073594ddd12af1b5f4
Divit <tekirukichi@gmail.com>
2020-11-11 14:02:28 +0530

    Completed Car Racing Game

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,8 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
+
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -91,8 +92,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 3650){
       gameState = 2;
+      player.rank += 1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +103,14 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank)
+    swal({
+      text: "Your rank is :"+player.rank,
+    })
+    // swal(player.rank)
+    //  alert(player.rank)
+    //if(butonClicked("OK")){
+      //alert.remove();
+   // }
   }
 }
